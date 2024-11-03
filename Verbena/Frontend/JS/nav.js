fef0dc25e4b97c23bfeee313cb746df38d2ac6a7
Mars-Phoenix-04 <marsphoenix2004@gmail.com>
2024-11-03 03:42:29 -0700

    Nav&Foot (Ahora si de verdad)

--- Verbena/Frontend/JS/nav.js
+++ Verbena/Frontend/JS/nav.js
@@ -1,18 +1,15 @@
-// Función para alternar el dropdown
 function toggleDropdown() {
-    var dropdown = document.getElementById("forosDropdown");
-    dropdown.classList.toggle("show");
-    const flecha = document.getElementById("foroflecha");
-    flecha.classList.toggle("active");
-}
-
-// Cierra el dropdown si se hace clic fuera del área de "Foros" o del mismo dropdown
-document.addEventListener('click', function(event) {
-    var dropdown = document.getElementById("forosDropdown");
-    var forosButton = document.getElementById("foros");
-
-    // Si el clic no es en el botón "Foros" o dentro del dropdown, cierra el dropdown
-    if (!forosButton.contains(event.target) && !dropdown.contains(event.target)) {
-        dropdown.classList.remove("show");
+    const dropdown = document.getElementById('forosDropdown');
+    
+    if (!dropdown.classList.contains('show')) {
+        dropdown.style.display = 'block';
+        setTimeout(() => {
+            dropdown.classList.add('show'); 
+        }, 10); 
+    } else {
+        dropdown.classList.remove('show');
+        setTimeout(() => {
+            dropdown.style.display = 'none'; 
+        }, 500); 
     }
-});
+}
