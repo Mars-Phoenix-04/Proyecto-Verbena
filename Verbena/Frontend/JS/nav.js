029b1b82d51b22a3eaac95f11510af2e2f18bbdf
Mars-Phoenix-04 <marsphoenix2004@gmail.com>
2024-11-02 22:15:29 -0600

    BarraNav & Footer

---
+++ Verbena/Frontend/JS/nav.js
@@ -0,0 +1,16 @@
+// Función para alternar el dropdown
+function toggleDropdown() {
+    var dropdown = document.getElementById("forosDropdown");
+    dropdown.classList.toggle("show");
+}
+
+// Cierra el dropdown si se hace clic fuera del área de "Foros" o del mismo dropdown
+document.addEventListener('click', function(event) {
+    var dropdown = document.getElementById("forosDropdown");
+    var forosButton = document.getElementById("foros");
+
+    // Si el clic no es en el botón "Foros" o dentro del dropdown, cierra el dropdown
+    if (!forosButton.contains(event.target) && !dropdown.contains(event.target)) {
+        dropdown.classList.remove("show");
+    }
+});
