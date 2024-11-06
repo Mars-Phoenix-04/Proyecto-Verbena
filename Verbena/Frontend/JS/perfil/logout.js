f38deecdd19515fbf48dbe428548030c9e6f4c9f
Mars-Phoenix-04 <marsphoenix2004@gmail.com>
2024-11-05 21:51:03 -0700

    MensajeDeSignOut

--- Verbena/Frontend/JS/perfil/logout.js
+++ Verbena/Frontend/JS/perfil/logout.js
@@ -1,3 +1,7 @@
 function logout() {
-    window.location.href = "../index.html"; 
+    const fuga = confirm('¿Estas seguro que deseas salir?');
+    if (fuga) {
+        alert('Muy bien, ahora serás redirigido a la página de inicio. Esperamos verte de vuelta pronto:)');
+        window.location.href = "../index.html";
+    } 
 }
