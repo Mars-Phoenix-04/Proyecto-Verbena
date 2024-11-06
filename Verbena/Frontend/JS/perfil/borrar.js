d4396f35db3fcb2281abef9de0b5c8c0f54905ab
Mars-Phoenix-04 <marsphoenix2004@gmail.com>
2024-11-05 23:36:51 -0700

    FooterFix

--- Verbena/Frontend/JS/perfil/borrar.js
+++ Verbena/Frontend/JS/perfil/borrar.js
@@ -1,8 +1,12 @@
 //Cambiar el color de fondo de la publicacion cuando se hace hover en el icono de borrar
 const posts = document.querySelectorAll('.post');
 const deleteButtons = document.querySelectorAll('.delete-button');
+const saves = document.querySelectorAll('.save');
+const desaves = document.querySelectorAll('.guard-button');
+
 deleteButtons.forEach((button, index) => {
 const post = posts[index]; 
+
 button.addEventListener('mouseenter', () => {
 post.style.backgroundColor = 'var(--color5)'; 
 });
@@ -10,10 +14,22 @@ button.addEventListener('mouseleave', () => {
 post.style.backgroundColor = 'var(--color1)'; 
 });
 });
+
 //Funcion para borrar nuestras publicaciones, aun falta vincular con la base de datos
 function borrarpost(postId) {
     const confirmarborro = confirm("¿Estás seguro de que deseas eliminar esta publicación? No hay vuelta atrás, una vez eliminada dejará de existir.");
         if (confirmarborro) {
+        const post = document.getElementById(postId);
+        if (post) {
+            post.remove(); 
+            alert("¡Publicación eliminada! Esperamos sigas comentando y participando:)");
+        }
+    }
+}
+//Funcion para desguardad publicaciones, aun falta vincular con la base de datos
+function desguardar(saveId) {
+    const confirmarborro2 = confirm("¿Estás seguro de que deseas eliminar esta publicación? No hay vuelta atrás, una vez eliminada dejará de existir.");
+        if (confirmarborro2) {
         const post = document.getElementById(postId);
         if (post) {
             post.remove(); 
